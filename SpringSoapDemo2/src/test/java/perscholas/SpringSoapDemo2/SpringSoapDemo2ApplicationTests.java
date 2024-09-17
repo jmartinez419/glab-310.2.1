da6a56f66de17256251746767dff91b71f5eafc4
jmartinez419 <justynmartinez419@gmail.com>
2024-09-17 12:25:43 -0400

    first commit

---
+++ SpringSoapDemo2/src/test/java/perscholas/SpringSoapDemo2/SpringSoapDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package perscholas.SpringSoapDemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
